a9f33757c60685a89654038c8fffe59bfef2eafd
pdogs232 <128694616+pdogs232@users.noreply.github.com>
2023-04-04 11:10:28 +1000

    ship types

--- normalMode.js
+++ normalMode.js
@@ -1,4 +1,32 @@
+var boatType = 0 //Type 1 = vertical Type 2 = horizontal Type 3 = L shape
+
+
+
 function cellClicked(tablecell){
+
+if (boatType == 1){
     alert(tablecell.id)
+    document.getElementById(tablecell.id).style.backgroundColor = "red"
+    document.getElementById(tablecell.id - 10).style.backgroundColor = "red"
+    document.getElementById(tablecell.id - 20).style.backgroundColor = "red"
+   
+}
+
+if (boatType == 2){
+    alert(tablecell.id)
+    document.getElementById(tablecell.id).style.backgroundColor = "red"
+    document.getElementById(tablecell.id - 1).style.backgroundColor = "red"
+    document.getElementById(tablecell.id + 1).style.backgroundColor = "red"
+}
+
+if (boatType == 3){
+    alert(tablecell.id)
+    document.getElementById(tablecell.id).style.backgroundColor = "red"
+    document.getElementById(tablecell.id - 10).style.backgroundColor = "red"
+    document.getElementById(tablecell.id + 10).style.backgroundColor = "red"
+}
+    
+   
+
 
 }
