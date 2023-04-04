17885b4afada94841fb98f1f120f1c6331feb9fc
pdogs232 <128694616+pdogs232@users.noreply.github.com>
2023-04-04 11:29:17 +1000

    rotate keybind made
    hi hunter

--- normalMode.js
+++ normalMode.js
@@ -1,26 +1,42 @@
-var boatType = 0 //Type 1 = vertical Type 2 = horizontal Type 3 = L shape
+var boatRotation = 1 //Type 1 = vertical Type 2 = horizontal Type 3 = L shape
+
+
+document.addEventListener("keydown", function (e){
+    if (e.keyCode === 82) {
+        rotateShip();
+        
+    }
+});
+
+function rotateShip(){
+    if (boatRotation !== 1 ){
+        boatRotation = 1
+    } else{
+        boatRotation = 2
+    }
+}
 
 
 
 function cellClicked(tablecell){
 
-if (boatType == 1){
-    alert(tablecell.id)
+if (boatRotation == 1){
+    //alert(tablecell.id)
     document.getElementById(tablecell.id).style.backgroundColor = "red"
     document.getElementById(tablecell.id - 10).style.backgroundColor = "red"
     document.getElementById(tablecell.id - -10).style.backgroundColor = "red"
    
 }
 
-if (boatType == 2){
-    alert(tablecell.id)
+if (boatRotation == 2){
+    //alert(tablecell.id)
     document.getElementById(tablecell.id).style.backgroundColor = "red"
     document.getElementById(tablecell.id - 1).style.backgroundColor = "red"
     document.getElementById(tablecell.id - -1).style.backgroundColor = "red"
 }
 
-if (boatType == 3){
-    alert(tablecell.id)
+if (boatRotation == 3){
+    //alert(tablecell.id)
     document.getElementById(tablecell.id).style.backgroundColor = "red"
     document.getElementById(tablecell.id - 10).style.backgroundColor = "red"
     document.getElementById(tablecell.id + 10).style.backgroundColor = "red"
