8b2d960ed4e0e3308f6647082ad73518015f7903
pdogs232 <128694616+pdogs232@users.noreply.github.com>
2023-04-27 10:48:26 +1000

    edge invalids
    finalised

--- normalMode.js
+++ normalMode.js
@@ -26,7 +26,7 @@ function cellClicked(tablecell){
     alert(tablecell.id)
     var boatPos = tablecell.id
 
-if (boatRotation == 1 && (boatPos.substring(1, 2)!=="0") && (boatPos.substring(1, 2)!=="9")){
+if (boatRotation == 1 && (boatPos.substring(0, 1)!=="0") && (boatPos.substring(0, 1)!=="9") ){
     //alert(tablecell.id)
     document.getElementById(tablecell.id).style.backgroundColor = "red"
     document.getElementById(tablecell.id - 10).style.backgroundColor = "red"
@@ -34,7 +34,7 @@ if (boatRotation == 1 && (boatPos.substring(1, 2)!=="0") && (boatPos.substring(1
    
 } else 
 
-if (boatRotation == 2 && (boatPos.substring(1, 2)!=="9" && (boatPos.substring(0, 1)!=="0"))){
+if (boatRotation == 2 && (boatPos.substring(1, 2)!=="9" && (boatPos.substring(1, 2)!=="0")) && boatPos!=="0"){
     //alert(tablecell.id)
     document.getElementById(tablecell.id).style.backgroundColor = "red"
     document.getElementById(tablecell.id - 1).style.backgroundColor = "red"
