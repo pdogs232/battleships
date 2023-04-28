e0f707b520a541aa9fbfd17b0448c46c2b6d93c3
HunterFa <85271801+HunterFa@users.noreply.github.com>
2023-04-28 12:20:11 +1000

    Update normalMode.js
    Fixed problems with row a not working and optimised a bit

--- normalMode.js
+++ normalMode.js
@@ -24,6 +24,7 @@ function rotateShip(){
 
 
 function cellClicked(tablecell){
+
     //alert(tablecell.id)
     var boatPos = tablecell.id //position of center cell of boat
     var boatPosTop = boatPos - 10
@@ -31,27 +32,38 @@ function cellClicked(tablecell){
     var boatPosLef = boatPos - 1
     var boatPosRig = boatPos - - 1
 
-    
+    if (boatPosTop<10) {
+        boatPosTop.toString()
+        boatPosTop = "0" + boatPosTop
+    }
 
-if (boatRotation == 1 && (boatPos.substring(0, 1)!== "0") && (boatPos.substring(0, 1)!=="9"  ) ){ //vertical object 
-    //alert(tablecell.id)
-    document.getElementById(tablecell.id).style.backgroundColor = "grey"
-    document.getElementById(tablecell.id - -10).style.backgroundColor = "grey"
-    document.getElementById(tablecell.id - 10).style.backgroundColor = "grey"
+    if (boatPos<8) {
+        boatPosLef.toString()
+        boatPosRig.toString()
+        boatPosLef = "0" + boatPosLef
+        boatPosRig = "0" + boatPosRig
+
+    }
+
+    if (boatRotation == 1 && (boatPos.substring(0, 1)!== "0") && (boatPos.substring(0, 1)!=="9"  ) ){ //vertical object 
+        //alert(tablecell.id)
+        document.getElementById(boatPos).style.backgroundColor = "grey"
+        document.getElementById(boatPosBot).style.backgroundColor = "grey"
+        document.getElementById(boatPosTop).style.backgroundColor = "grey"
 
    boatCount[boatNum] = boatPosTop.toString() + boatPos.toString() + boatPosBot.toString()
    alert (boatCount[boatNum])
     boatNum++
 } else 
 
-if (boatRotation == 2 && (boatPos.substring(1, 2)!=="9" && (boatPos.substring(1, 2)!=="0")) && boatPos!=="9" && boatPos!=="0"){ // horizontal object
+    if (boatRotation == 2 && (boatPos.substring(1, 2)!=="9" && (boatPos.substring(1, 2)!=="0")) && boatPos!=="9" && boatPos!=="0"){ // horizontal object
     //alert(tablecell.id)
-    document.getElementById(tablecell.id).style.backgroundColor = "grey"
-    document.getElementById(tablecell.id - 1).style.backgroundColor = "grey"
-    document.getElementById(tablecell.id - -1).style.backgroundColor = "grey"
-    boatCount[boatNum] = boatPosLef.toString() + boatPos.toString() + boatPosRig.toString()
-    alert (boatCount[boatNum])
-    boatNum++
+        document.getElementById(boatPos).style.backgroundColor = "grey"
+        document.getElementById(boatPosLef).style.backgroundColor = "grey"
+        document.getElementById(boatPosRig).style.backgroundColor = "grey"
+        boatCount[boatNum] = boatPosLef.toString() + boatPos.toString() + boatPosRig.toString()
+        alert (boatCount[boatNum])
+        boatNum++
 
 
 } else 
