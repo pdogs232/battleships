b5a67406958278b9f1446303f73db358a0a9be03
HunterFa <85271801+HunterFa@users.noreply.github.com>
2023-04-28 18:16:12 +1000

    Unfinished boundary conditions for boats but on the right track

--- normalMode.js
+++ normalMode.js
@@ -1,7 +1,8 @@
 var boatRotation = 1 //Type 1 = vertical Type 2 = horizontal 
 var boatPos;
-var boatCount = []
-var boatNum = 0
+var boatCount = 0
+var boatNum = []
+var boatNum2 = []
 
 
 document.addEventListener("keydown", function (e){
@@ -28,9 +29,13 @@ function cellClicked(tablecell){
     //alert(tablecell.id)
     var boatPos = tablecell.id //position of center cell of boat
     var boatPosTop = boatPos - 10
-    var boatPosBot = boatPos - - 10
-    var boatPosLef = boatPos - 1
-    var boatPosRig = boatPos - - 1
+    var boatPosBottom = boatPos - - 10
+    var boatPosLeft = boatPos - 1
+    var boatPosRight = boatPos - - 1
+    var invalid = false
+    boatCount = boatCount + 1
+   boatNum[boatCount] = boatPosTop + boatPos.toString() + boatPosBottom.toString()
+   boatNum[boatCount] = boatPosLeft + boatPos.toString() + boatPosRight
 
     if (boatPosTop<10) {
         boatPosTop.toString()
@@ -38,37 +43,58 @@ function cellClicked(tablecell){
     }
 
     if (boatPos<8) {
-        boatPosLef.toString()
-        boatPosRig.toString()
-        boatPosLef = "0" + boatPosLef
-        boatPosRig = "0" + boatPosRig
+        boatPosLeft.toString()
+        boatPosRight.toString()
+        boatPosLeft = "0" + boatPosLeft
+        boatPosRight = "0" + boatPosRight
 
     }
 
-    if (boatRotation == 1 && (boatPos.substring(0, 1)!== "0") && (boatPos.substring(0, 1)!=="9"  ) ){ //vertical object 
+    shipParamiters()
+
+    if (boatRotation == 1 && (boatPos.substring(0, 1)!== "0") && (boatPos.substring(0, 1)!=="9") && invalid == false ){ //vertical object 
         //alert(tablecell.id)
         document.getElementById(boatPos).style.backgroundColor = "grey"
-        document.getElementById(boatPosBot).style.backgroundColor = "grey"
+        document.getElementById(boatPosBottom).style.backgroundColor = "grey"
         document.getElementById(boatPosTop).style.backgroundColor = "grey"
 
-   boatCount[boatNum] = boatPosTop.toString() + boatPos.toString() + boatPosBot.toString()
-   alert (boatCount[boatNum])
-    boatNum++
+
+   alert (boatNum[boatCount])
+
+
+
+
 } else 
 
     if (boatRotation == 2 && (boatPos.substring(1, 2)!=="9" && (boatPos.substring(1, 2)!=="0")) && boatPos!=="9" && boatPos!=="0"){ // horizontal object
     //alert(tablecell.id)
         document.getElementById(boatPos).style.backgroundColor = "grey"
-        document.getElementById(boatPosLef).style.backgroundColor = "grey"
-        document.getElementById(boatPosRig).style.backgroundColor = "grey"
-        boatCount[boatNum] = boatPosLef.toString() + boatPos.toString() + boatPosRig.toString()
-        alert (boatCount[boatNum])
-        boatNum++
+        document.getElementById(boatPosLeft).style.backgroundColor = "grey"
+        document.getElementById(boatPosRight).style.backgroundColor = "grey"
+        boatNumt[boatCount] = boatPosLeft + boatPos.toString() + boatPosRight
+        alert (boatNum[boatCount])
+
 
 
 } else 
+    { alert("invalid")}
 
-{ alert("invalid")}
+
+    function shipParamiters(){
+        for (i=0;i<5;i++) {
+            if (boatNum[boatCount] == boatNum2[i]) {
+                invalid = true
+            }
+            else if(boatNum[boatCount].substring(0,2) == boatNum2[i].substring(4,6)){
+                invalid = true
+            }
+
+
+
+        }
+    }
+
+    boatNum2[boatCount] =  boatNum[boatCount]
 
 
     
