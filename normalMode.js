0bb64863cd4c1c58f62aefd2fadc10a5021010d6
HunterFa <85271801+HunterFa@users.noreply.github.com>
2023-05-02 21:25:39 +1000

    bot boat pick complete PETER NEEDS TO CHANGE VARIABLE NAMES

--- normalMode.js
+++ normalMode.js
@@ -3,6 +3,9 @@ var boatPos;
 var boatCount = 0
 var boatNum = []
 var boatNum2 = ["0","0","0","0","0"]
+var bbc = 0;
+var bbNum = [];
+var bbNum2 = ["0","0","0","0","0"]
 
 document.getElementById("guessDiv").style.display = "none"
 
@@ -37,7 +40,6 @@ function cellClicked(tablecell){
     var boatPosLeft = boatPos - 1
     var boatPosRight = boatPos - - 1
     var invalid = false
-    var p = 0
     boatCount = boatCount + 1
     
     if (boatCount<6) {
@@ -97,7 +99,7 @@ function cellClicked(tablecell){
         }
 
         function shipParameters(){
-            for (i=0;i<boatCount;i++) {
+            for (i=1;i<boatCount;i++) {
                 var q = boatNum[boatCount]
                 var x = boatNum2[i]
 
@@ -140,18 +142,124 @@ function cellClicked(tablecell){
             }
         }
     
-        if (invalid = false) {
+
         boatNum2[boatCount] =  boatNum[boatCount]
+    }
+}
+
+function submit() {
+    if (boatCount = 5) {
+        document.getElementById("playerDiv").style.display = "none"
+        document.getElementById("guessDiv").style.display = "block"
+        
+        for (f = 1; f<6; f++) {
+            var bp = Math.floor((Math.random() * 99) + 0) //position of center cell of boat
+            var bpt = bp - 10
+            var bpb = bp - - 10
+            var bpl = bp - 1
+            var bpr = bp - - 1
+            var binvalid= false
+            var bboatRotation = Math.floor((Math.random() * 2) + 1)
+            bbc = bbc + 1
+            bp = bp.toString()
+
+            if (bp<10){
+                bp = "0"+bp
+            }
+        
+            if (bpt<10) {
+                bpt = "0" + bpt
+            }
+        
+            if (bp<9) {
+                bpl = "0" + bpl.toString()
+                bpr = "0" + bpr.toString()
+        
+            }
+                
+            if (bboatRotation == 1 && (bp.substring(0, 1)!== "0") && (bp.substring(0, 1)!=="9") && binvalid == false ){
+                bbNum[bbc] = bpt.toString() + bp.toString() + bpb.toString();
+                bshipParameters();
+            }
+                
+                
+            if (bboatRotation == 2 && (bp.substring(1, 2)!=="9" && (bp.substring(1, 2)!=="0")) && bp!=="9" && bp!=="0" && binvalid == false){
+                bbNum[bbc] = bpl.toString() + bp.toString() + bpr.toString();
+                bshipParameters();
+            }
+
+            if (bboatRotation == 1 && (bp.substring(0, 1)!== "0") && (bp.substring(0, 1)!=="9") && binvalid == false ){ //vertical object 
+                alert (bbNum[bbc])
+        
+            } else 
+        
+            if (bboatRotation == 2 && (bp.substring(1, 2)!=="9" && (bp.substring(1, 2)!=="0")) && bp!=="9" && bp!=="0" && binvalid == false){ // horizontal object
+                alert (bbNum[bbc])
+        
+        
+        
+            } 
+            else {         
+                alert("invalid")
+                bbNum[bbc] = null   
+                bbNum2[bbc] = "0"
+                bbc = bbc - 1
+            
+            }
+            function bshipParameters(){
+                for (i=1;i<bbc;i++) {
+                    var y = bbNum[bbc]
+                    var z = bbNum2[i]
+    
+                    if( y.substring(0,2) == z.substring(0,2) ){
+                        binvalid = true
+                    }   
+    
+                    else if( y.substring(0,2) == z.substring(2,4) ){
+                        binvalid = true
+                    }
+                    
+                    else if( y.substring(0,2) == z.substring(4,6) ){
+                        binvalid = true
+                    }
+    
+                    else if( y.substring(2,4) == z.substring(0,2) ){
+                        binvalid = true
+                    }
+    
+                    else if( y.substring(2,4) == z.substring(2,4) ){
+                        binvalid = true
+                    }
+    
+                    else if( y.substring(2,4) == z.substring(4,6) ){
+                        binvalid = true
+                    }
+    
+                    else if( y.substring(4,6) == z.substring(0,2) ){
+                        binvalid = true
+                    }
+                    else if( y.substring(4,6) == z.substring(2,4) ){
+                        binvalid = true
+                    }
+    
+                    else if( y.substring(4,6) == z.substring(4,6) ){
+                        binvalid = true
+                    }
+    
+    
+                }
+            }
+
+            bbNum2[bbc] =  bbNum[bbc]
+            f = bbc
+
+
+
         }
+
     }
 
-    function submit() {
-        if (boatCount = 5) {
-            document.getElementById("playerDiv").style.display = "none"
-            document.getElementById("guessDiv").style.display = "block"
-        }
-    }
+}
    
 
 
-}
